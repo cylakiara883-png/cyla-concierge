737af97d88d48c250fb607dd74a406182958b57f
cylakiara883-png <cylakiara883@gmail.com>
2026-07-31 17:47:47 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        }
+    });
+});
+
+elements.forEach(element => {
+    observer.observe(element);
+});
