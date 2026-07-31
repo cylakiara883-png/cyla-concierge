75f77b35f628ce0a24f809ed83be6a75bc4c2769
cylakiara883-png <cylakiara883@gmail.com>
2026-07-31 18:46:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ const observer = new IntersectionObserver((entries) => {
 elements.forEach(element => {
     observer.observe(element);
 });
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+setInterval(() => {
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    slides[currentSlide].classList.add("active");
+
+}, 5000);
