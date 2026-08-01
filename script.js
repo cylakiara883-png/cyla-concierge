4e8014d7cb82c13927a09b291c6b9e4dc562fce5
cylakiara883-png <cylakiara883@gmail.com>
2026-08-01 20:16:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const slides = document.querySelectorAll(".slide");
-
 let index = 0;
 
 function changerImage() {
-
     slides[index].classList.remove("active");
-
-    index++;
-
-    if (index >= slides.length) {
-        index = 0;
-    }
-
+    index = (index + 1) % slides.length;
     slides[index].classList.add("active");
 }
 
 setInterval(changerImage, 5000);
+
+// Animation des éléments
+
+const elements = document.querySelectorAll(".fade-in");
+
+function apparition() {
+    elements.forEach((element) => {
+        const position = element.getBoundingClientRect().top;
+        const hauteur = window.innerHeight;
+
+        if (position < hauteur - 100) {
+            element.classList.add("visible");
+        }
+    });
+}
+
+window.addEventListener("scroll", apparition);
+window.addEventListener("load", apparition);
