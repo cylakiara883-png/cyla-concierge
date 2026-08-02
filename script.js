189dae54fd7ceb777727dc8264f9f6add3269144
cylakiara883-png <cylakiara883@gmail.com>
2026-08-02 22:29:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,32 @@ ${message}`;
 
     window.open(url, "_blank");
 }
+
+function envoyerWhatsApp() {
+
+    const nom = document.getElementById("nom").value;
+    const email = document.getElementById("email").value;
+    const telephone = document.getElementById("telephone").value;
+    const vehicule = document.getElementById("vehicule").value;
+    const debut = document.getElementById("debut").value;
+    const fin = document.getElementById("fin").value;
+    const message = document.getElementById("message").value;
+
+    const texte = `Bonjour Cyla Concierge,
+
+Nom : ${nom}
+Email : ${email}
+Téléphone : ${telephone}
+
+Véhicule : ${vehicule}
+
+Date de début : ${debut}
+Date de fin : ${fin}
+
+Demandes particulières :
+${message}`;
+
+    const url = "https://wa.me/243810061720?text=" + encodeURIComponent(texte);
+
+    window.open(url, "_blank");
+}
