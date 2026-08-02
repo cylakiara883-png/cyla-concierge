d64618f362520f2163ba414bc5467e6c91155cc3
cylakiara883-png <cylakiara883@gmail.com>
2026-08-02 19:02:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,37 @@ function apparition() {
 
 window.addEventListener("scroll", apparition);
 window.addEventListener("load", apparition);
+function envoyerWhatsApp() {
+
+    const nom = document.getElementById("nom").value;
+    const email = document.getElementById("email").value;
+    const telephone = document.getElementById("telephone").value;
+    const vehicule = document.getElementById("vehicule").value;
+    const debut = document.getElementById("debut").value;
+    const fin = document.getElementById("fin").value;
+    const message = document.getElementById("message").value;
+
+    const texte =
+`Bonjour Cyla Concierge,
+
+Nom : ${nom}
+
+Email : ${email}
+
+Téléphone : ${telephone}
+
+Véhicule : ${vehicule}
+
+Du : ${debut}
+
+Au : ${fin}
+
+Demande :
+
+${message}`;
+
+    const url =
+`https://wa.me/243810061720?text=${encodeURIComponent(texte)}`;
+
+    window.open(url, "_blank");
+}
